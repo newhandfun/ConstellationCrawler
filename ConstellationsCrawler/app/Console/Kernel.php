777de1f8d2bb034d88f1d12b64a23a205634e8c6
蔡良方 <sf1onile@gmail.com>
2021-05-22 21:44:52 +0800

    bug(schedule):let crawler run each hour.

--- ConstellationsCrawler/app/Console/Kernel.php
+++ ConstellationsCrawler/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(ConstellationCrawler::class)
-            ->dailyAt('02:00');
+            ->hourly();
     }
 
     /**
