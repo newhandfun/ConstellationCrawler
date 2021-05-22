0c589be04eed15ca5cec25ebdcc63d1725f5ed0c
蔡良方 <sf1onile@gmail.com>
2021-05-22 12:41:32 +0800

    feat($view):user login page
    add zh-tw translate file.
    set web api routes.

--- ConstellationsCrawler/app/Providers/AppServiceProvider.php
+++ ConstellationsCrawler/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\UserLoginRepository;
+use App\Repositories\Contracts\UserRegisterRepository;
+use App\Repositories\EloquentUserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserLoginRepository::class,EloquentUserRepository::class);
+        $this->app->bind(UserRegisterRepository::class,EloquentUserRepository::class);
     }
 
     /**
